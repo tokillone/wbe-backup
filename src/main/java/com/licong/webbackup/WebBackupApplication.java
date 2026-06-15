e5f72a290477b145a9918c7430ce54b252543002
李聪 <cong.li@bluefocus.cn>
2026-06-15 16:05:37 +0800

    feature(new project):第一次提交

---
+++ src/main/java/com/licong/webbackup/WebBackupApplication.java
@@ -0,0 +1,13 @@
+package com.licong.webbackup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBackupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/licong/webbackup/WebBackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licong.webbackup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
