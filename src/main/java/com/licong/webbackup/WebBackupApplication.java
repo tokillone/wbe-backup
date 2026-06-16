f1e96b3f303d642ae7786eec018a0b74153be8ae
李聪 <cong.li@bluefocus.cn>
2026-06-16 14:47:57 +0800

    feature（登陆注册）：实现了登陆

--- src/main/java/com/licong/webbackup/WebBackupApplication.java
+++ src/main/java/com/licong/webbackup/WebBackupApplication.java
@@ -2,8 +2,10 @@ package com.licong.webbackup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.licong.webbackup.mapper")
 public class WebBackupApplication {
 
     public static void main(String[] args) {
